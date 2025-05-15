ecce3d935b255714efe87f25c107f5d197b905ed
Alise <ipa23.k.strazdina@vtdt.edu.lv>
2025-05-15 13:12:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Dice from "./Dice";
+
+function App() {
+
+  return (
+    <>
+      <h1>Gājienu palīgs</h1>
+      <Dice />
+    </>
+  );
+ <article className="dice"><button onClick={rollDice}>Mest</button></article>;
+
+}
+
+export default App;
+              
